1eb23a9e9efa50db4317ef4b445dcc6294eddaad
sudhakar96536-droid <sudhakar96536@gmail.com>
2026-05-18 14:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def webhook():
 # 🔹 Run server
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=False)
